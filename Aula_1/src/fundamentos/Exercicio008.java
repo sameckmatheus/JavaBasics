1824a09eec9834635338ed966040072042535452
sameckmatheus <sameckmatheus>
2023-11-21 11:57:25 -0300

    fix: challenge

--- Aula_1/src/fundamentos/Exercicio008.java
+++ Aula_1/src/fundamentos/Exercicio008.java
@@ -1,12 +1,17 @@
 package fundamentos;
 import java.util.Scanner;
 
-public class Exercicio007 {
+public class Exercicio008 {
 	public static void main(String[] args) {
 		Scanner entrada = new Scanner(System.in);
 		System.out.println("Digite um número: ");
+		
 		double resp = entrada.nextDouble();
-		System.out.println(resp);
+		
+		if (resp < 0) {
+			System.out.println("O número é negativo.");
+		} else {
+			System.out.println("O número é positivo.");
+		}
 	}
 }
-
