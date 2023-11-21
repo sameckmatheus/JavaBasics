48c102d8259b5a43ded094d2dd8f1cc0206a86f2
sameckmatheus <sameckmatheus>
2023-11-21 10:57:34 -0300

    chore: add java project setup

---
+++ Aula_1/src/fundamentos/variaveis.java
@@ -0,0 +1,15 @@
+package fundamentos;
+
+public class variaveis {
+	public static void main(String[] args) {
+		String sayHello = "Hello, World!";
+		System.out.println(sayHello);
+		
+		int x = 10;
+		
+		System.out.println(x++);
+		System.out.println(++x);
+		System.out.println(x--);
+		System.out.println(--x);
+	}
+}
